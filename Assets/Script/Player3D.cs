5e558e2bb182df66b0d21022061973383600908a
rim1822rum <rim1822rum@gmail.com>
2025-07-29 18:32:54 +0900

    初回コミット

---
+++ Assets/Script/Player3D.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player3D : MonoBehaviour
+{
+    public float MoveSpeed = 10f;
+    public float DeadZone = 0.2f;
+
+    [SerializeField, Header("ジャンプ速度")]
+    private float _jumpSpeed;
+    private Vector2 _moveInput;
+    private Rigidbody _rigid;
+    private bool _bJump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HitFloor();
+    }
+
+    private void FixedUpdate()
+    {
+        // 前方
+        if (_moveInput.y > DeadZone || _moveInput.y < -DeadZone)
+        {
+            transform.Translate(Vector3.forward * _moveInput.y * Time.deltaTime);
+        }
+        // 左右
+        if (_moveInput.x > DeadZone || _moveInput.x < -DeadZone)
+        {
+            transform.Translate(Vector3.right * _moveInput.x * Time.deltaTime);
+        }
+    }
+
+    //プレイヤーが床に接しているかどうかを判定するメソッド
+    private void HitFloor()
+    {
+        // 地面との判定に使うレイヤーを取得（Layer名: "floor"）
+        int layerMask = LayerMask.GetMask("floor");
+
+        // レイを飛ばす起点（プレイヤーの足元）
+        Vector3 rayOrigin = transform.position;
+
+        // プレイヤーの半分の高さぶん下にレイを飛ばす
+        float rayDistance = (transform.localScale.y / 2f) + 0.1f;
+
+        // 下方向にRaycastして床に当たるかチェック
+        if (Physics.Raycast(rayOrigin, Vector3.down, out RaycastHit hit, rayDistance, layerMask))
+        {
+            if (_bJump && hit.collider.CompareTag("floor"))
+            {
+                _bJump = false; // 着地と判定
+            }
+        }
+        else
+        {
+            _bJump = true; // 床に触れていない = 空中
+        }
+    }
+
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        //もしジャンプ操作が行われていないまたは、_bJumpがtrueの時はreturn以降の処理を行わない
+        if (!context.performed || _bJump)
+        {
+            return;
+        }
+
+        //上方向に_jumpSpeed分徐々に加速させる
+        _rigid.AddForce(Vector2.up * _jumpSpeed, ForceMode.Impulse);
+    }
+}
---
+++ Assets/Script/ScriptChanger.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptChanger : MonoBehaviour
+{
+    private Player2D _moveTypeA;
+    private Player3D _moveTypeB;
+
+    private bool move = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // コンポーネント取得
+        _moveTypeA = GetComponent<Player2D>();
+        _moveTypeB = GetComponent<Player3D>();
+
+        // 初期状態設定
+        _moveTypeA.enabled = move;
+        _moveTypeB.enabled = !move;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            // 切り替え
+            move = !move;
+
+            _moveTypeA.enabled = move;
+            _moveTypeB.enabled = !move;
+        }
+    }
+}
